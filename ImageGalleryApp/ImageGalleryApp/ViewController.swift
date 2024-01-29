1c35a406c39407d0f19d2022c08c59e0bc00b0e9
Oleg Kasarin <>
2024-01-29 15:41:16 +0100

    Setup project

---
+++ ImageGalleryApp/ImageGalleryApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ImageGalleryApp
+//
+//  Created by Oleg Kasarin on 29/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
