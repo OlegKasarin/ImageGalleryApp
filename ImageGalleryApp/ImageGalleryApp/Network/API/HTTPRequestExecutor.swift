245a5ad712aed9d5f0a3a94a4e5baa709fe855c7
Oleg Kasarin <>
2024-01-29 23:31:09 +0100

    Add Network layer

---
+++ ImageGalleryApp/ImageGalleryApp/Network/API/HTTPRequestExecutor.swift
@@ -0,0 +1,60 @@
+//
+//  HTTPRequestExecutor.swift
+//  ImageGalleryApp
+//
+//  Created by Oleg Kasarin on 29/01/2024.
+//
+
+import Foundation
+
+typealias URLResponsePayload = (data: Data, response: URLResponse)
+
+protocol HTTPRequestExecutorProtocol {
+    func execute<T: Codable>(urlString: String) async throws -> T
+}
+
+final class HTTPRequestExecutor: HTTPRequestExecutorProtocol {
+    func execute<T: Codable>(urlString: String) async throws -> T {
+        guard let url = URL(string: urlString) else {
+            throw "Error: wrong url"
+        }
+        
+        let payload = try await fetchData(url: url)
+        guard (payload.response as? HTTPURLResponse)?.statusCode == 200 else {
+            throw "Error: bad response"
+        }
+        
+        return try JSONDecoder().decode(T.self, from: payload.data)
+    }
+    
+    private func fetchData(url: URL) async throws -> URLResponsePayload {
+        if #available(iOS 14.0.0, *) {
+            let request = URLRequest(url: url)
+            return try await URLSession.shared.data(for: request)
+        } else {
+            return try await URLSession.shared.data(from: url)
+        }
+    }
+}
+
+// MARK: - Extensions
+
+extension URLSession {
+    @available(iOS, deprecated: 15.0, message: "This extension is no longer necessary. Use API built into SDK")
+    func data(from url: URL) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: url) { data, response, error in
+                guard let data = data, let response = response else {
+                    let error = error ?? URLError(.badServerResponse)
+                    return continuation.resume(throwing: error)
+                }
+                
+                continuation.resume(returning: (data, response))
+            }
+            
+            task.resume()
+        }
+    }
+}
+
+extension String: Error { }
---
+++ ImageGalleryApp/ImageGalleryApp/Network/NetworkAssembly.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkAssembly.swift
+//  ImageGalleryApp
+//
+//  Created by Oleg Kasarin on 29/01/2024.
+//
+
+import Foundation
+
+struct NetworkAssembly {
+    static var requestExecutor: RequestExecutorProtocol {
+        RequestExecutor(builder: URLRequestBuilder())
+    }
+}
